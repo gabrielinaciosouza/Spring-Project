cc44aafb787152a0107c0958d67bda59511509b5
Gabriel Inacio <gabriel.sert9@gmail.com>
2020-05-21 21:35:19 -0300

    Criando operacao de instanciacao

--- src/main/java/com/gabriel/projetospring/ProjetospringApplication.java
+++ src/main/java/com/gabriel/projetospring/ProjetospringApplication.java
@@ -1,13 +1,34 @@
 package com.gabriel.projetospring;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.gabriel.projetospring.domain.Categoria;
+import com.gabriel.projetospring.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetospringApplication {
+public class ProjetospringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetospringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
 	}
 
 }
