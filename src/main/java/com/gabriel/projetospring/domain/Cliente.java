724828be97c03204524b2160c7dbda883bdc8346
Gabriel Inacio <gabriel.sert9@gmail.com>
2020-05-23 22:30:46 -0300

    PUT,DELETE e GET para Cliente

--- src/main/java/com/gabriel/projetospring/domain/Cliente.java
+++ src/main/java/com/gabriel/projetospring/domain/Cliente.java
@@ -49,7 +49,7 @@ public class Cliente implements Serializable {
 		this.id = id;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo == null) ? null :tipo.getCod();
 		this.nome = nome;
 	}
 
