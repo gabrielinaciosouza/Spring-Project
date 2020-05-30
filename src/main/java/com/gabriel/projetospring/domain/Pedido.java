347f56b7d642831738237096e7eafe67cf7f8753
Gabriel Inacio <gabriel.sert9@gmail.com>
2020-05-30 12:20:32 -0300

    Revert "gabri@LAPTOP-Q2R569E6 MINGW64 ~/OneDrive/Área de Trabalho/Curso spring boot/projetospring (master)"
    This reverts commit 94d3cdc6ee0011c39ff4d28e0aa9be4332b18c7a.

--- src/main/java/com/gabriel/projetospring/domain/Pedido.java
+++ src/main/java/com/gabriel/projetospring/domain/Pedido.java
@@ -1,11 +1,8 @@
 package com.gabriel.projetospring.domain;
 
 import java.io.Serializable;
-import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
-import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -137,31 +134,5 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
-
-	@Override
-	public String toString() {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
-		StringBuilder builder = new StringBuilder();
-		builder.append("Pedido número: ");
-		builder.append(getId());
-		builder.append(" Instante: ");
-		builder.append(sdf.format(getInstante()));
-		builder.append(", Cliente: ");
-		builder.append(getCliente().getNome());
-		builder.append(", Situação do pagamento: ");
-		builder.append(getPagamento().getEstado().getDescricao());
-		builder.append("\nDetalhes:\n");
-		for (ItemPedido ip : getItens()) {
-			builder.append(ip.toString());
-		}
-		builder.append("Valor total: ");
-		builder.append(nf.format(getValorTotal()));
-		
-		
-		return builder.toString();
-	}
-	
-	
 
 }
--- src/main/java/com/gabriel/projetospring/service/PedidoService.java
+++ src/main/java/com/gabriel/projetospring/service/PedidoService.java
@@ -29,8 +29,6 @@ public class PedidoService {
 	private ProdutoService produtoService;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	@Autowired
-	private ClienteService clienteService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -42,7 +40,6 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
-		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -53,13 +50,11 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setProduto(produtoService.find(ip.getProduto().getId()));
-			ip.setPreco(ip.getProduto().getPreco());
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
 			ip.setPedido(obj);
 			
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
-		System.out.println(obj);
 		return obj;
 	}
 
